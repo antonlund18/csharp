29e68ca5e478939a57b49807a807797653fe8013
Anton Lund <antonlund95@gmail.com>
2021-02-03 12:10:17 +0100

    Random & Age

---
+++ Lektion 2/Age/Age/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void calcAge(int birthyear, out int age)
+        {
+            age = DateTime.Now.Year - birthyear;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your birthyear:");
+            int birthyear = int.Parse(Console.ReadLine());
+            int age;
+            calcAge(birthyear, out age);
+            Console.WriteLine("Your age is: " + age);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lektion 2/RandomList/RandomList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("{0,15:N0}", rand.Next());
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("{0,15:N0}", rand.NextDouble() * 100);
+            }
+            Console.ReadKey();
+            // NextDouble producerer doubles i stedet for Integers
+        }
+    }
+}
